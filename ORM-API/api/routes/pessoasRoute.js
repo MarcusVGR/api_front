799e97ba57957ba516952ff88228e86f4df0b4df
MarcusVGR <marcusvgr@gmail.com>
2022-03-29 09:46:14 -0300

    master

--- ORM-API/api/routes/pessoasRoute.js
+++ ORM-API/api/routes/pessoasRoute.js
@@ -9,13 +9,9 @@ router.post('/pessoas', PessoasController.criaPessoa)
 router.put('/pessoas/:id', PessoasController.atualizaPessoa)
 router.delete('/pessoas/:id', PessoasController.apagaPessoa)
 
-router.get('/pessoas/:alunoId/matricula/:matriculaId',
-    PessoasController.pegaUmaMatricula)
-router.post('/pessoas/:alunoId/matricula',
-    PessoasController.criaMatricula)
-router.put('/pessoas/:alunoId/matricula/:matriculaId',
-    PessoasController.atualizaMatricula)
-router.delete('/pessoas/:alunoId/matricula/:matriculaId',
-     PessoasController.apagaMatricula)
+router.get('/pessoas/:alunoId/matricula/:matriculaId', PessoasController.pegaUmaMatricula)
+router.post('/pessoas/:alunoId/matricula', PessoasController.criaMatricula)
+router.put('/pessoas/:alunoId/matricula/:matriculaId', PessoasController.atualizaMatricula)
+router.delete('/pessoas/:alunoId/matricula/:matriculaId', PessoasController.apagaMatricula)
 
 module.exports = router
